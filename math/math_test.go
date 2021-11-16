7552e2feef3816b2cfb19ab6306622a43fa80623
mahesh <mail.mahesh.lk@gmail.com>
2021-11-16 11:45:40 +0800

    lifecycle

--- math/math_test.go
+++ math/math_test.go
@@ -2,12 +2,23 @@ package math
 
 import "testing"
 
+// arg1 means argument 1 and arg2 means argument 2, and the expected stands for the 'result we expect'
+type addTest struct {
+	arg1, arg2, expected int
+}
+
+var addTests = []addTest{
+	addTest{2, 3, 5},
+	addTest{4, 8, 12},
+	addTest{6, 9, 15},
+	addTest{3, 10, 13},
+}
+
 func TestAdd(t *testing.T) {
 
-	got := Add(4, 6)
-	want := 10
-
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
+	for _, test := range addTests {
+		if output := Add(test.arg1, test.arg2); output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
 	}
 }
