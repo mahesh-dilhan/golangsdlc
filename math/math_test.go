558638ba75ef3ca808e9af35bc6da4344df3521c
mahesh <mail.mahesh.lk@gmail.com>
2021-11-19 12:03:43 +0800

    lifecycle

--- math/math_test.go
+++ math/math_test.go
@@ -1,6 +1,9 @@
 package math
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // arg1 means argument 1 and arg2 means argument 2, and the expected stands for the 'result we expect'
 type addTest struct {
@@ -26,5 +29,30 @@ func TestAdd(t *testing.T) {
 func BenchmarkAdd(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Add(4, 6)
+	}
+}
+
+var table = []struct {
+	x    int
+	y    int
+	want int
+}{
+	{2, 2, 4},
+	{5, 3, 8},
+	{8, 4, 12},
+	{12, 5, 17},
+}
+
+func TestSumParalel(t *testing.T) {
+	t.Parallel()
+	for _, row := range table {
+		testName := fmt.Sprintf("Test %d+%d", row.x, row.y)
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
+			got := Add(row.x, row.y)
+			if got != row.want {
+				t.Errorf("Test fail! want: '%d', got: '%d'", row.want, got)
+			}
+		})
 	}
 }
